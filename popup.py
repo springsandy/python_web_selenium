f4cc39e3101dcaddc083db2471bae514342249bc
s2018w26 <s2018w26>
2022-02-06 22:39:02 +0900

    창을 화면 가운데에 띄우기

--- popup.py
+++ popup.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
 
 
 class MyApp(QWidget):
@@ -9,9 +9,15 @@ class MyApp(QWidget):
 
     def initUI(self):
         self.setWindowTitle('idol search')
-        self.move(300, 300)
         self.resize(400, 200)
+        self.center()
         self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 
 if __name__ == '__main__':
