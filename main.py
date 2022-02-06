44d13e1c7e1c0e647913fff52480f6e6c79c14f0
s2018w26 <s2018w26>
2022-02-06 20:45:47 +0900

    원하는 데이터들 불러와 확인

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ search = driver.find_element_by_id('query')
 search.send_keys(idol)
 
 driver.find_element_by_class_name('btn_submit').click()
+
+profile_data = driver.find_elements_by_xpath('//dl[@class="info txt_3"]//div[@class="info_group"]')
+for data in profile_data:
+    print(data.find_element_by_tag_name('dt').get_property('textContent') + " : " + data.find_element_by_tag_name('dd').get_property('textContent'))
