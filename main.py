23f419d06c1bcce96ff834e456eeb937cb19bbb3
s2018w26 <s2018w26>
2022-02-06 20:44:35 +0900

    naver 검색 후 검색 버튼 클릭

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from selenium import webdriver
 
+# idol = input("찾고 싶은 아이돌을 입력해주세요!\n")
+idol = "비투비"
+
 driver = webdriver.Chrome()
 driver.get("http://www.naver.com/")
+
+search = driver.find_element_by_id('query')
+search.send_keys(idol)
+
+driver.find_element_by_class_name('btn_submit').click()
